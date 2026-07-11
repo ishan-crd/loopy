e6fe6eec3ad87555e2ced2da2a59766c70118f42
ishan-crd <ishangupta3121@gmail.com>
2026-07-11 14:13:38 +0530

    Initial Commit

---
+++ loopyTests/loopyTests.swift
@@ -0,0 +1,17 @@
+//
+//  loopyTests.swift
+//  loopyTests
+//
+//  Created by Ishan Gupta on 11/07/26.
+//
+
+import Testing
+@testable import loopy
+
+struct loopyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
